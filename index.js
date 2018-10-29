a74c276c3bb6e95fca420a04abbbdac8d9e1f3c3
jun68ykt <jun68ykt@teratail>
2018-10-30 05:00:31 +0900

    入力されたときの処理修正

--- index.js
+++ index.js
@@ -4,22 +4,13 @@ $(function(){
   （実際には多くの値があるので、このような書き方ではない方法を知りたいです）
   */
 
-  $(".name").on('keyup',function(){
-    var input_name1 = $('#name1').val();
-    var input_name2 = $('#name2').val();
-    var input_name3 = $('#name3').val();
-    var output_names = input_name1+','+input_name2+','+input_name3;
-    $('#result_name').val(output_names);
-  });
+  const inputData = function() {
+    const clazz = $(this).attr('class');
+    const values = $(`.${clazz}`).toArray().map(e => e.value);
+    $(`#result-${clazz}`).val(values.join(','));
+  };
 
-  $(".valu").on('keyup',function(){
-    var input_valu1 = $('#valu1').val();
-    var input_valu2 = $('#valu2').val();
-    var input_valu3 = $('#valu3').val();
-    var output_valus = input_valu1+','+input_valu2+','+input_valu3;
-    $('#result_valu').val(output_valus);
-  });
-
+  $('.data input').on('keyup change', inputData);
 
   /*
   ボタンクリックで【入力エリア】の追加と削除
@@ -30,8 +21,10 @@ $(function(){
     // 一行目をクローン
     const newData = $('.data:first-of-type').clone();
 
-    // input の値を初期化
-    $('input', newData).val('');
+    // input の値を初期化し、イベントハンドラ設定
+    $('input', newData)
+      .val('')
+      .on('keyup change', inputData);
 
     // data-table に追加
     $('.data-table').append(newData);
